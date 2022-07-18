4d4bff6c35549c0adb7f468e27301c8338766bcd
Adwaver4157 <adwaver4157@gmail.com>
2022-07-19 05:41:21 +0900

    fix: fix fetch in service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -17,6 +17,31 @@ self.addEventListener('install', function(event) {
 
 // リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;     // if valid response is found in cache return it
+          } else {
+            return fetch(event.request)     //fetch from internet
+              .then(function(res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());    //save the response for future
+                    return res;   // return the fetched data
+                  })
+              })
+              .catch(function(err) {       // fallback mechanism
+                return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                  .then(function(cache) {
+                    return cache.match('/offline.html');
+                  });
+              });
+          }
+        })
+    );
+  });   
+/* self.addEventListener('fetch', function(event) {
     console.log('fetch');
     event.respondWith(caches.match(event.request).then(function(response){
       // Cache hit - return response
@@ -36,4 +61,4 @@ self.addEventListener('fetch', function(event) {
         return response;
       })
     }));
-})
+}) */
